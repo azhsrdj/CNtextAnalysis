ec3a2615f05338be5a83bbe3ecc5451ee60b0572
azhsrdj <chenziwu90@hotmail.com>
2024-01-10 01:33:42 +0800

    update

--- DataMerge/DataMerge_README.md
+++ DataMerge/DataMerge_README.md
@@ -1,4 +1,5 @@
 # DataMerge
+
 县域 企业 行业 数据匹配和合并
 
 ## 前言
@@ -19,45 +20,7 @@
 
 
 
-## 设计思路
+## 文件
 
-
-
-## 文件准备
-
-本项目重要的文件有如下几个：
-
-1. ciku.txt 记录所要统计的关键词，程序需要用到这个文件两次，第一次是将其添加进jieba分词的基础词库，因为有些关键词可能不在jieba的基础词库中，第二次是在keyword_count时用来统计文本中关键词出现的次数。
-2. cn_stopwords.txt 记录了分词时需要停用的停用词。
-
-本项目需要根据实际情况修改源代码的地方：
-
-由于读取源文件和输出目录被代码写死，在不同计算机下使用本项目需要修改源文件目录和输出文件目录：
-
-main下
-
-```python
-files = glob.glob('/Users/wcz/Downloads/B2021txt/*.txt') 
-```
-
-记录了源文件目录，这里的txt文档是pdf2txt转化将pdf年报文档转化成的txt文档。
-
-```python
-output_csv = '/Users/wcz/Downloads/C2021txt/' + os.path.splitext(os.path.basename(file_path))[0] + '.csv' 
-```
-
-记录了输出文件目录和输出文件的命名规则，转化后是一份csv文档。包含 Keyword，Count  两列，最后一行记录Total
-
-```python
-with open('/Users/wcz/Downloads/word2evc/TxTAlysis/ciku.txt', 'r', encoding='utf-8') as f:
-        keywords = [line.strip() for line in f]
-```
-
-这行代码
-
-```python
-stopword_file = '/Users/wcz/Downloads/word2evc/TxTAlysis/cn_stopwords.txt'
-```
-
-记录了停用词目录
+example_debug.py
 
--- DataMerge/example_debug.py
+++ DataMerge/example_debug.py
@@ -9,36 +9,33 @@ Created on Mon Jan  8 21:41:30 2024
 import numpy as np
 import pandas as pd
 from numpy import *
-
+#%% 读取表格
 df2 = pd.read_csv('/Users/wcz/Downloads/表2.csv')
 df_tableX1old = pd.read_csv('/Users/wcz/Downloads/表X1_old.csv')
-
-# t = df2.columns[2:].tolist()
-# for col in t:
-#     df_tableX1old[col] = pd.NA  
-
-df2_clear  = df2.drop(df2[df2['CompanyName']!='X1'].index)
-df_tableX1old_clear = df_tableX1old.dropna(axis=0,how='any',subset=['Treat'])
-df2_clear = df2_clear.append([df2_clear]*(df_tableX1old_clear.shape[0]-1))
-df_tableX1old_clear = df_tableX1old_clear.reset_index(drop=True)
-df2_clear = df2_clear.reset_index(drop=True)
-df_tableX1old_clear = pd.concat([df_tableX1old_clear,df2_clear.iloc[:,2:]],axis=1)
-print(df_tableX1old_clear)
-
-CountryName = df_tableX1old_clear['CountryName'].T.tolist()
-
-# =============================================================================
-# 分界线
-# =============================================================================
 df1 = pd.read_csv('/Users/wcz/Downloads/表1.csv')
+df1.iloc[:,2:]=0 # 初始化，所有行业的联系都是0
 
-df1.iloc[:,2:]=0
 
-temp = df1.loc[df1['CountryName'].isin(CountryName)]
+#%% 针对表X1_old的处理
+df2_clear  = df2.drop(df2[df2['CompanyName']!='X1'].index) # 将企业“X1”保留，其他行删除
+df_tableX1old_clear = df_tableX1old.dropna(axis=0,how='any',subset=['Treat']) # tableX1中Treat为0的行删除
+df2_clear = df2_clear.append([df2_clear]*(df_tableX1old_clear.shape[0]-1)) # df2的行数与df_tableX1old_clear 对齐
+df_tableX1old_clear = df_tableX1old_clear.reset_index(drop=True) 
+df2_clear = df2_clear.reset_index(drop=True)
+# 重新index
+df_tableX1old_clear = pd.concat([df_tableX1old_clear,df2_clear.iloc[:,2:]],axis=1)
+# df2 的行业表单拼接到 df_tableX1old_clear 中
+CountryName = df_tableX1old_clear['CountryName'].T.tolist() # 获得df_tableX1old_clear中包含的CountryName，下面要用到
 
-df1 = df1.drop(temp.index,axis=0)
+#%%  df_tableX1old_clear 合并到表1
 
-temp2 = temp.iloc[:,2:]
+ # 
+
+temp = df1.loc[df1['CountryName'].isin(CountryName)] 
+df1 = df1.drop(temp.index,axis=0) 
+# 剪切，提去“CountryName” 变量中的行
+
+temp2 = temp.iloc[:,2:] # 切数据，只保留行业
 x = temp2.index.to_series()
 x = pd.DataFrame(x).reset_index(drop=True)
 df_tableX1old_clear = pd.concat([df_tableX1old_clear,x],axis=1)
@@ -49,8 +46,8 @@ temp2 = temp2.add(df_tableX1old_clear.iloc[:,2:],fill_value=0)
 
 temp = pd.concat([temp.iloc[:,0:2],temp2],axis=1)
 
-
 df1 = pd.concat([df1,temp],axis=0)
-print(temp2.index)
 
 df1.sort_index(inplace=True)
+
+# 用一个特殊技巧合并
